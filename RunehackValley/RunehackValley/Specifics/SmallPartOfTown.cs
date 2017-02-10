c12b9505c2e4328cd5efd9de740a4cc9da3bba46
noah reinagel <noah.r9@gmail.com>
2017-02-09 20:45:36 -0700

    Add Background Track

--- RunehackValley/RunehackValley/Specifics/SmallPartOfTown.cs
+++ RunehackValley/RunehackValley/Specifics/SmallPartOfTown.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 using RunehackValley.Engine;
 using RunehackValley.MonoGame;
 
@@ -8,15 +9,20 @@ namespace RunehackValley.Specifics
     public class SmallPartOfTown : IGameView
     {
         private Texture2D mapTexture;
+        private Song backgroundTrack;
 
         public void LoadContent()
         {
             mapTexture = new LoadedTexture("Farm.png").Get();
+            backgroundTrack = new LoadedSong("FarmMusic").Get();
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(backgroundTrack);
         }
 
         public void UnloadContent()
         {
             mapTexture.Dispose();
+            backgroundTrack.Dispose();
         }
 
         public void Update(GameTime deltaTime)
